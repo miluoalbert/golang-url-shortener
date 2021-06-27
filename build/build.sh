2ff1e85dfa653c45cb184f586d5fb378fb22031a
Tingcheng Wu <miluo.albert@gmail.com>
2021-06-27 23:36:21 +0800

    Fix link error

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 rm -rf docker_releases
 mkdir docker_releases
-go build -o docker_releases/golang-url-shortener ./cmd/golang-url-shortener
+CGO_ENABLED=0 go build -o docker_releases/golang-url-shortener ./cmd/golang-url-shortener
 
 docker build -t miluoabt/golang_url_shortener -f build/Dockerfile.amd64 .
